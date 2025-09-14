f568d89d9f60b44ee17ff2e8fc6ba4e112cd2c14
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-09-14 11:58:08 -0600

    django stuff

---
+++ frontend/front/classes/ReportType.py
@@ -0,0 +1,6 @@
+from enum import Enum
+class ReportType(Enum):
+    NO_STOCK = "no_stock"
+    OVER_10 ="over 10usd"
+    MOST_POPULAR_COLOR = "most popular color"
+    MOST_EXPENSIVE = "most expensive"
